c608c80a6291bd33d5b74c33a57cf71632b0af97
Alan <alan.edward.martin@gmail.com>
2020-04-28 10:55:55 -0400

    Updates

---
+++ precache-manifest.e841621b6bf788160de95d3d2b9dfa27.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aea36c5eccf83cfd3390feec235b35be",
+    "url": "/deploymentPractice/index.html"
+  },
+  {
+    "revision": "36c96065bc12d0aded35",
+    "url": "/deploymentPractice/static/css/main.6a934dfc.chunk.css"
+  },
+  {
+    "revision": "dbd56b10f501082fe7c0",
+    "url": "/deploymentPractice/static/js/2.a17f8560.chunk.js"
+  },
+  {
+    "revision": "4ae2e24854201e2c90f766e681fee893",
+    "url": "/deploymentPractice/static/js/2.a17f8560.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36c96065bc12d0aded35",
+    "url": "/deploymentPractice/static/js/main.5891a825.chunk.js"
+  },
+  {
+    "revision": "e157866002f2ed8c6abb",
+    "url": "/deploymentPractice/static/js/runtime-main.14c9f371.js"
+  }
+]);
